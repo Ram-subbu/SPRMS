bda51d3808ad9ce5cc2dfc63de4609feecffd918
ramsubbu <ramsubbu@BSS-004>
2026-05-04 15:30:01 +0530

    initial commit

--- obj/Debug/net8.0/SPRMS.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SPRMS.API.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Scholarship Profile and Resource Management System — RCSC")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0+9e6df678b5c641dfeeac54f30b943ec3de43adbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPRMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPRMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
